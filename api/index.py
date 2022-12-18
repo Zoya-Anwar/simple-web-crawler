b7a1bd7105bba3194c2f96dea4f970efd2668380
zoyaanwar@hotmail.com <zoyaanwar@hotmail.com>
2022-12-18 23:17:11 +0000

    reformat vercel.json

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+from web_crawler import web_crawl
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def web_scraper(url=None): 
+    if request.method == 'POST':
+        if url != "":
+            scraped = web_crawl(request.form['url'])
+            return render_template('index.html', url=url, scraped=scraped)
+    return render_template('index.html', url=url)
